3ef2273ef7cc2461475055a69a5eecf9f72496e4
Dany Dias <diasdany2708@gmail.com>
2022-02-25 12:24:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const body = document.querySelector('body');
+
+let you;
+let yourScore = 0;
+let opponent;
+let opponentScore = 0;
+let i;
+
+const choices = ["rock", "paper", "scissors", "shaft"];
+
+window.onload = function() {
+    for (i = 0; i < 3; i++) {
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png";
+        choice.addEventListener("click", selectChoice);
+        document.getElementById("choices").append(choice);
+    }
+};
+
+function selectChoice() {
+    you = this.id;
+    document.getElementById("your-choice").src = you + ".png";
+    opponent = choices[Math.floor(Math.random() * 4)];
+    document.getElementById("opponent-choice").src = opponent + ".png";
+    if (you == opponent) {
+        yourScore += 0;
+        opponentScore += 0;
+    }
+    else {
+        if (you == "rock") {
+            if (opponent == "scissors") {
+                yourScore += 1;
+            }
+            else if (opponent == "paper") {
+                opponentScore += 1;
+            }
+        }
+        else if (you == "scissors") {
+            if (opponent == "paper") {
+                yourScore += 1;
+            }
+            else if (opponent == "rock") {
+                opponentScore += 1;
+            }
+        }
+        else if (you == "paper") {
+            if (opponent == "rock") {
+                yourScore += 1;
+            }
+            else if (opponent == "scissors") {
+                opponentScore += 1;
+            }
+        }
+    }
+    document.querySelector("#your-score span").innerText = yourScore;
+    document.querySelector("#opponent-score span").innerText = opponentScore;
+    if (opponent == "shaft"){
+        opponentScore += 10;
+    }
+};
